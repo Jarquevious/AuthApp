472eb98ee8cbe4c9957a0d8d65ee4f410875cae6
Jarquevious <jarquevious@gmail.com>
2020-07-12 19:20:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+/*  EXPRESS SETUP  */
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+const bodyParser = require('body-parser');
+const expressSession = require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('App listening on port ' + port));
+
+const passport = require('passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* MONGOOSE SETUP */
+
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+mongoose.connect('mongodb://localhost/MyDatabase',
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+  username: String,
+  password: String
+});
+
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+/* PASSPORT LOCAL AUTHENTICATION */
+
+passport.use(UserDetails.createStrategy());
+
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
